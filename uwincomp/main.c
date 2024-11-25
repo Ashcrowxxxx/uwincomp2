f499af94553b91ec0bd3eccf0c7741217a804122
Siyi Zheng <million124@163.com>
2024-11-24 22:48:31 -0500

    Initial Commit

---
+++ uwincomp/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  uwincomp
+//
+//  Created by 郑斯贻 on 2024-11-24.
+//gggg
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
